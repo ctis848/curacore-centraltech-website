3bdc9205d6a3e10c8ba552bafe7be0dadc1c7dda
Folorunsho Olabisi <ctistechnologies@gmail.com>
2026-03-03 02:54:24 +0100

    Add supabase admin client

--- lib/supabase/supabaseAdmin.ts
+++ lib/supabase/supabaseAdmin.ts
@@ -2,10 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!, // must be added to Netlify env
   {
     auth: {
-      autoRefreshToken: false,
       persistSession: false,
     },
   }
