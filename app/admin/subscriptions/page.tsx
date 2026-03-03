087d04fdd0ea664391c22db840dc0b46936b30d8
Folorunsho Olabisi <ctistechnologies@gmail.com>
2026-03-03 10:21:15 +0100

    Add default export for subscriptions admin page

---
+++ app/admin/subscriptions/page.tsx
@@ -0,0 +1,8 @@
+export default function SubscriptionsPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold mb-4">Subscriptions</h1>
+      <p>This admin page is under construction.</p>
+    </div>
+  );
+}
