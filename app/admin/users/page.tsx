340fa0603449ab4560c87985b4b0b0d25fd9ddd4
Folorunsho Olabisi <ctistechnologies@gmail.com>
2026-03-03 10:17:49 +0100

    Add default export for users admin page

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,8 @@
+export default function UsersPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold mb-4">Users</h1>
+      <p>This admin page is under construction.</p>
+    </div>
+  );
+}
